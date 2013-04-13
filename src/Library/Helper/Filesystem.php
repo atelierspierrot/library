87e950e3261d7b430f21d71d1c7d660bbca819a8
Piero Wbmstr <me@e-piwi.fr>
2013-04-14 00:07:46 +0200

    New filesystem helper
    For now, it just estimate the relative path between two absolute paths
    - upgrade to version 1.0.1

---
+++ src/Library/Helper/Filesystem.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * PHP Library package of Les Ateliers Pierrot
+ * Copyleft (c) 2013 Pierre Cassat and contributors
+ * <www.ateliers-pierrot.fr> - <contact@ateliers-pierrot.fr>
+ * License GPL-3.0 <http://www.opensource.org/licenses/gpl-3.0.html>
+ * Sources <https://github.com/atelierspierrot/library>
+ */
+
+namespace Library\Helper;
+ 
+/**
+ * File helper
+ *
+ * As for all helpers, all methods are statics.
+ *
+ * For convenience, the best practice is to use:
+ *
+ *     use Library\Helper\Filesystem as FilesystemHelper;
+ *
+ * @author      Piero Wbmstr <piero.wbmstr@gmail.com>
+ */
+class Filesystem
+{
+
+    /**
+     * Returns a relative path between two filesystem realpaths
+     *
+     * @param string $from The absolute path to work from
+     * @param string $to The absolute path to resolve from `$from`
+     * @return string The relative path from `$from` to `$to`
+     */
+    public static function resolveRelatedPath($from, $to)
+    {
+        $from_parts = array_filter( explode('/', $from) );
+        $to_parts = array_filter( explode('/', $to) );
+        foreach($from_parts as $i=>$path) {
+            if (in_array($path, $to_parts)) {
+                $from_parts[$i] = null;
+                $to_parts[$i] = null;
+            }
+        }
+        $from_parts = array_filter($from_parts);
+        $to_parts = array_filter($to_parts);
+        for($i=0; $i<count($from_parts); $i++) {
+            array_unshift($to_parts, '..');
+        }
+/*
+        foreach($from_parts as $path) {
+            array_unshift($to_parts, $path);
+        }
+*/
+        return join('/', $to_parts);
+    }
+
+}
+
+// Endfile
