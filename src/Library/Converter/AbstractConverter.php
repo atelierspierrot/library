dda8de6c6f494c0f7105255fa894a9104ce50c6c
Piero Wbmstr <me@e-piwi.fr>
2013-07-11 23:24:02 +0200

    New converter and populating the README

---
+++ src/Library/Converter/AbstractConverter.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * PHP Library package of Les Ateliers Pierrot
+ * Copyleft (c) 2013 Pierre Cassat and contributors
+ * <www.ateliers-pierrot.fr> - <contact@ateliers-pierrot.fr>
+ * License GPL-3.0 <http://www.opensource.org/licenses/gpl-3.0.html>
+ * Sources <https://github.com/atelierspierrot/library>
+ */
+
+namespace Library\Converter;
+
+/**
+ */
+abstract class AbstractConverter
+{
+
+	public static function convert($content);
+}
+
+// Endfile
