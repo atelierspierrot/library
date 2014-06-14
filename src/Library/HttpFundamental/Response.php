48a0d66e6c2c27b626b9f3782f707b8d83b15de0
Piero Wbmstr <me@e-piwi.fr>
2014-06-14 09:50:42 +0200

    new piwi github username

--- src/Library/HttpFundamental/Response.php
+++ src/Library/HttpFundamental/Response.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * PHP Library package of Les Ateliers Pierrot
- * Copyleft (c) 2013 Pierre Cassat and contributors
+ * Copyleft (c) 2013-2014 Pierre Cassat and contributors
  * <www.ateliers-pierrot.fr> - <contact@ateliers-pierrot.fr>
  * License GPL-3.0 <http://www.opensource.org/licenses/gpl-3.0.html>
- * Sources <https://github.com/atelierspierrot/library>
+ * Sources <http://github.com/atelierspierrot/library>
  */
 namespace Library\HttpFundamental;
 
@@ -16,7 +16,7 @@ use \Patterns\Interfaces\ResponseInterface;
  *
  * This is the global response of the application
  *
- * @author      Piero Wbmstr <piero.wbmstr@gmail.com>
+ * @author      Piero Wbmstr <me@e-piwi.fr>
  */
 class Response
 {
