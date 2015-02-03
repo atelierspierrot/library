6b950a6bf8fe1558235e45792aabf3985a701867
piwi <me@e-piwi.fr>
2015-02-03 23:51:07 +0100

    new organization of tests

--- tests/testsLibrary/TestCase.php
+++ tests/testsLibrary/TestCase.php
@@ -27,4 +27,27 @@ abstract class TestCase
     extends \PHPUnit_Framework_TestCase
 {
 
+    public $lorem_ipsum =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    public $short_lorem_ipsum =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+    public $long_lorem_ipsum =
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.";
+
+    /**
+     * @param $classname
+     * @param $meth
+     * @param null $default
+     */
+    public function checkHelperMethodNoArg($classname, $meth, $default = null)
+    {
+        try {
+            $this->assertEquals(
+                $default, call_user_func(array($classname, $meth))
+            );
+        } catch (\Exception $e) {
+            $this->fail("Failure of '$classname::$meth()' with no arg!");
+        }
+    }
+
 }
